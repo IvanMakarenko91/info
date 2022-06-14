e08e4f607eaec0881cbacbdfe0f606b42dcc2a75
Makarenko Ivan <ivan.makarenko@free.fr>
2022-06-14 09:30:21 +0200

    Initialisation React + styled components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Ukraine from './components/Ukraine';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <Ukraine />
       </header>
     </div>
   );
---
+++ src/components/Ukraine.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components/macro';
+
+const Ukraine = () => {
+  return (
+    <Titre>ukraine</Titre>
+  )
+}
+
+const Titre = styled.div`
+background-color: red;
+font-weight: bold;
+`
+
+export default Ukraine
