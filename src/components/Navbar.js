78cafb587fe88ed9f3ea4f3ac0d2f8a48c3e88b0
Makarenko Ivan <ivan.makarenko@free.fr>
2022-06-14 15:25:21 +0200

    Navbar Responsive / Hook

---
+++ src/components/Navbar.js
@@ -0,0 +1,53 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React, {useState, useEffect} from 'react';
+import './Navbar.css';
+
+const Navbar = () => {
+
+  const [toggleMenu, setToggleMenu] = useState(false);
+  const [largeur, setLargeur] = useState(window.innerWidth);
+
+  const toggleNavSmallScreen = () => {
+    setToggleMenu(!toggleMenu);
+  }
+
+  useEffect(() => {
+
+    const changeWidth = () => {
+      setLargeur(window.innerWidth);
+
+      if (window.innerWidth > 909) {
+        setToggleMenu(false);
+      }
+    }
+
+    window.addEventListener('resize', changeWidth);
+
+    return () => {
+      window.removeEventListener('resize',changeWidth);
+    }
+
+  }, [])
+  
+  return (
+    <nav>
+      {(toggleMenu || largeur > 909) && (
+        <ul className='show'>
+        <li className="active"><a href="#">Actualité</a></li>
+        <li><a href="#">Inscription </a></li>
+        <li><a href="#">Connexion</a></li>
+        <li className='interieur'><i class="fa-brands fa-facebook-f"></i></li>
+        <li className='interieur'><i class="fa-brands fa-twitter"></i></li>
+        <li className='interieur'><i class="fa-brands fa-instagram"></i></li>
+        <li className='interieur'><i class="fa-brands fa-linkedin-in"></i></li>
+      </ul>
+      )}
+    <label className="logo">Info</label>
+    <label id="icon">
+      <i className="fa-solid fa-bars" onClick={toggleNavSmallScreen}></i>
+    </label>
+  </nav>
+  )
+}
+
+export default Navbar
