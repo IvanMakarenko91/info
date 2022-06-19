121dc7ca341d3dcdaba1a2db7118c218f1bdfa6b
Makarenko Ivan <ivan.makarenko@free.fr>
2022-06-19 10:05:04 +0200

    Active link

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import './App.css';
 // import Ukraine from './components/Ukraine';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, NavLink } from "react-router-dom";
 
 function App() {
   return (
     <div>
         <Navbar />
         <navbar>
-          <Link to="/politique" className='separation'>Politique</Link>
-          <Link to="/canicule" className='separation'>Canicule</Link>
-          <Link to="/ukraine" className='separation'>Ukraine</Link>
-          <Link to="/inflation" className='separation'>Inflation</Link>
+          <NavLink to="/politique" className='separation'>Politique</NavLink>
+          <NavLink to="/canicule" className='separation'>Canicule</NavLink>
+          <NavLink to="/ukraine" className='separation'>Ukraine</NavLink>
+          <NavLink to="/inflation" className='separation'>Inflation</NavLink>
         </navbar>
       <Outlet />
       <Footer />
